4f1cf1602474f5bea962a08a4e979485e1120e67
Marcin-Klima <mnklima95@gmail.com>
2021-11-11 14:16:56 +0100

    Application downloads data through http.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <curl/curl.h>
+#include <fstream>
 
-int main()
+size_t write_data(char* ptr, size_t size, size_t nmemb, std::ofstream& fileHandler)
 {
-    std::cout << "Hello, World!" << std::endl;
+    size_t startPos = fileHandler.tellp();
+    fileHandler.write(ptr, nmemb);
+    size_t currentPos = fileHandler.tellp();
+    return currentPos - startPos;
+}
+
+int main(int argc, char** argv)
+{
+    CURL* curl;
+    curl = curl_easy_init();
+
+    if (curl)
+    {
+        std::ofstream file("myFace.omg", std::ios::binary | std::ios::app);
+
+        const std::string url("https://avatars.githubusercontent.com/u/15244432?v=4");
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
+
+        CURLcode curlResponse;
+        curlResponse = curl_easy_perform(curl);
+        std::cout << "Curl response code: " << curlResponse << std::endl;
+
+        long httpResponseCode = 0;
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpResponseCode);
+        std::cout << "Http response: " << httpResponseCode << std::endl;
+
+        curl_easy_cleanup(curl);
+    }
+    else
+    {
+        std::cout << "cannot init curl." << std::endl;
+    }
     return 0;
 }
