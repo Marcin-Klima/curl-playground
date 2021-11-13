dafad600ecd186d704063af70931c029a04d0d9d
Marcin-Klima <mnklima95@gmail.com>
2021-11-13 14:06:59 +0100

    Software now downloads simple page. Added conan dependency manager.

---
+++ README.md
@@ -0,0 +1,1 @@
+conan install -s build_type=Release -s arch=x86_64 -s compiler=clang -s os=Linux -if conanfiles/release .
--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,30 @@
 #include <curl/curl.h>
 #include <fstream>
 
-size_t write_data(char* ptr, size_t size, size_t nmemb, std::ofstream& fileHandler)
+size_t write_data(char* ptr, size_t size, size_t nmemb, std::string& htmlResponse)
 {
-    size_t startPos = fileHandler.tellp();
-    fileHandler.write(ptr, nmemb);
-    size_t currentPos = fileHandler.tellp();
-    return currentPos - startPos;
+    htmlResponse.append(ptr);
+    return htmlResponse.length();
 }
 
-int main(int argc, char** argv)
+bool executeHttpQuery(std::string url, const std::string& path, std::string* htmlResponse)
 {
     CURL* curl;
     curl = curl_easy_init();
+    CURLcode curlResponse;
+    long httpResponseCode = 0;
 
     if (curl)
     {
-        std::ofstream file("myFace.omg", std::ios::binary | std::ios::app);
+        url.append(path);
 
-        const std::string url("https://avatars.githubusercontent.com/u/15244432?v=4");
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &htmlResponse);
 
-        CURLcode curlResponse;
         curlResponse = curl_easy_perform(curl);
         std::cout << "Curl response code: " << curlResponse << std::endl;
 
-        long httpResponseCode = 0;
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpResponseCode);
         std::cout << "Http response: " << httpResponseCode << std::endl;
 
@@ -37,6 +34,36 @@ int main(int argc, char** argv)
     else
     {
         std::cout << "cannot init curl." << std::endl;
+        return false;
     }
+
+    return httpResponseCode == 200 && curlResponse == CURLE_OK;
+}
+
+int main(int argc, char** argv)
+{
+
+    std::string url("https://en.pons.com/translate/german-polish/");
+
+    std::string command = "";
+    while(command != "exit")
+    {
+        command.clear();
+        std::cin >> command;
+        if(command.starts_with("download "))
+        {
+            std::string path = command.substr(9);
+            std::string htmlDocumentResponse;
+            if(executeHttpQuery(url, path, &htmlDocumentResponse))
+            {
+                if(htmlDocumentResponse.find("rom first") != std::string::npos)
+                {
+                    std::cout << "founded!" << std::endl;
+                }
+            }
+        }
+    }
+
+
     return 0;
 }
